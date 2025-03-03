1864e99d9fdfeb6bc972d6b79c3652715a4d6fbc
sachin kunwar <sachinkunwar284@gmail.com>
2025-03-03 14:34:04 +0545

    Feature:Make Some Changes In Navbar and HeroSection

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Menu, School } from "lucide-react";
+import { GraduationCap, Menu } from "lucide-react";
 import React, { useEffect } from "react";
 import {
   DropdownMenu,
@@ -47,10 +47,10 @@ const Navbar = () => {
       {/* Desktop */}
       <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full">
         <div className="flex items-center gap-2">
-          <School size={"30"} />
+        <GraduationCap size={"40"} />
           <Link to="/">
-            <h1 className="hidden md:block font-extrabold text-2xl">
-            SkillMantra
+            <h1 className="hidden md:block font-extrabold text-2xl text-blue-500">
+            Skill<span className="text-green-500">Mantra</span>
             </h1>
           </Link>
         </div>
@@ -103,7 +103,7 @@ const Navbar = () => {
       </div>
       {/* Mobile device  */}
       <div className="flex md:hidden items-center justify-between px-4 h-full">
-        <h1 className="font-extrabold text-2xl">SkillMantra</h1>
+        <h1 className="font-extrabold text-2xl text-blue-500">Skill<span className="text-green-500">Mantra</span></h1>
         <MobileNavbar user={user}/>
       </div>
     </div>
@@ -128,7 +128,7 @@ const MobileNavbar = ({user}) => {
       </SheetTrigger>
       <SheetContent className="flex flex-col">
         <SheetHeader className="flex flex-row items-center justify-between mt-2">
-          <SheetTitle> <Link to="/">SkillMantra</Link></SheetTitle>
+          <SheetTitle> <Link to="/">Skill<span className="text-green-500">Mantra</span></Link></SheetTitle>
           <DarkMode />
         </SheetHeader>
         <Separator className="mr-2" />
--- client/src/pages/student/HeroSection.jsx
+++ client/src/pages/student/HeroSection.jsx
@@ -18,10 +18,10 @@ const navigate = useNavigate();
     <div className="relative bg-gradient-to-r from-blue-500 to bg-indigo-600 dark:from-gray-800 dark:to-gray-900 py-24 px-4 text-center">
       <div className="max-w-3xl mx-auto">
         <h1 className="text-white text-4xl font-bold mb-4">
-          Find the Best Courses for You
+          No.1 Platform For Enhancing Skill In <span className="text-red-400">Nepal</span>
         </h1>
         <p className="text-gray-200 dark:text-gray-400 mb-8">
-          Discover, Learn, and Upskill with our wide range of courses
+          Grow Your Skills,<span className="text-green-400">Build Your Future</span>
         </p>
 
         <form onSubmit={searchHandler} className="flex items-center bg-white dark:bg-gray-800 rounded-full shadow-lg overflow-hidden max-w-xl mx-auto mb-6">
